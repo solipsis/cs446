79e3adf7ad20f3d6cbc20bd6b1bc6b3350cf73a9
David Alexander <dalexand@mymail.mines.edu>
2014-01-18 21:42:03 -0700

    level 3 done

--- unit2/rubywarrior/the-kid-beginner/player.rb
+++ unit2/rubywarrior/the-kid-beginner/player.rb
@@ -1,10 +1,15 @@
 class Player
+  
   def play_turn(warrior)
     # add your code here
+    
     if (warrior.feel.enemy?)
     	warrior.attack!
+    elsif (warrior.health < 7)
+    	warrior.rest!
     else
     	warrior.walk!
+    	
     end
 
   end
