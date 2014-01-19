51b38ae7dbd16c8456c56316eba34989afc432f4
David Alexander <dalexand@mymail.mines.edu>
2014-01-18 22:09:14 -0700

    finished level 4

--- unit2/rubywarrior/the-kid-beginner/player.rb
+++ unit2/rubywarrior/the-kid-beginner/player.rb
@@ -1,16 +1,27 @@
 class Player
   
+  def initialize()
+    @health = 0
+  end
+  #@rest = true
+
   def play_turn(warrior)
     # add your code here
-    
+    rest = true
+    puts "@health:  #{@health} warhealth: #{warrior.health}"
+    if (@health > warrior.health)
+      rest = false
+    end
+    @health = warrior.health
     if (warrior.feel.enemy?)
     	warrior.attack!
-    elsif (warrior.health < 7)
+    elsif (warrior.health < 13 && (rest))
     	warrior.rest!
     else
     	warrior.walk!
-    	
     end
+
+
 
   end
 end
