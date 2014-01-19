c2b98e470b79def8add60bc4482cc3cfd831c01e
David Alexander <dalexand@mymail.mines.edu>
2014-01-19 00:58:04 -0700

    level 9 complete. epic mode unlocked

--- unit2/rubywarrior/the-kid-beginner/player.rb
+++ unit2/rubywarrior/the-kid-beginner/player.rb
@@ -8,6 +8,9 @@ class Player
     @yOffset = 0
     @captives = 1
     @reachedBack = false
+    # hash that holds list of enemies that should be approached
+    @meleeEnemies = Hash.new()
+    @meleeEnemies = {"Thick Sludge" => true, "Archer" => true, "Captive" => true,}
   end
   
   def beingAttacked?(wHealth) 
@@ -25,41 +28,56 @@ class Player
     return false
   end
 
+  def gatherInfo()
+    #warrior.look
+    #puts warrior.look
+  end
+
+
+  # look for objects of interest. go left first but prioritize captives
+  # stop healing after 2 enemies killed
+  
+
+
   def play_turn(warrior)
     # add your code here
-
+    gatherInfo()
+    puts warrior.look
     beingAttacked = beingAttacked?(warrior.health)
     lowHealth = lowHealth?(warrior.health)
-    rest = true
-
-
-
-
-    #add check to see if remaning monsters. if not dont rest.
     
-    #if (warrior.beingAttacked?())
-    #  rest = false
-    #end
+
 
     puts "@health:  #{@health} warhealth: #{warrior.health}"
     
     @health = warrior.health
   
+    puts warrior.look[0].to_s
+    puts warrior.look[1].to_s
+    puts warrior.look[2].to_s
 
     if (warrior.feel.captive?)
       warrior.rescue!
+    #elsif ((warrior.look[1].to_s == "Thick Sludge"))
+     # puts "WIZARD YONUTOTUHNTOU"
+    elsif ((!@meleeEnemies[warrior.look[0].to_s]) and (!@meleeEnemies[warrior.look[1].to_s]) and ((warrior.look[1].to_s == "Wizard") or warrior.look[2].to_s == "Wizard"))
+      warrior.shoot!
     elsif (warrior.feel.enemy?)
     	warrior.attack!
+    elsif (warrior.feel(:backward).enemy?)
+      warrior.pivot!
     elsif (lowHealth && (!beingAttacked))
     	warrior.rest!
     elsif (warrior.feel(:backward).captive?)
       warrior.rescue!(:backward)
     else
-      if (!@reachedBack)
-        warrior.walk!(:backward)
-        if (warrior.feel(:backward).wall?)
-          @reachedBack = true
-        end
+      #if (!@reachedBack)
+      #  warrior.walk!(:backward)
+      #  if (warrior.feel(:backward).wall?)
+      #    @reachedBack = true
+      #  end
+      if (warrior.feel.wall?) 
+        warrior.pivot!
       else
         if (beingAttacked && (warrior.health < 8))
           warrior.walk!(:backward)
