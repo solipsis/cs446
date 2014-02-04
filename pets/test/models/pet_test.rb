8d993c202ca0de3c8b8e0675a850a0168b5348d6
David Alexander <dalexand@mymail.mines.edu>
2014-02-04 10:19:35 -0700

    basic unit testing

--- pets/test/models/pet_test.rb
+++ pets/test/models/pet_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "pet attributes cant be empty" do
+  	pet = Pet.new
+  	assert pet.invalid?
+  	assert pet.errors[:name].any?
+  	assert pet.errors[:species].any?
+  	assert pet.errors[:age].any?
+  	assert pet.errors[:image_url].any?
+  end
+
+  test "age must be greater than 0" do
+  	pet = Pet.new(name: 'fido', species: 'shark', image_url: 'shark.jpg')
+  	pet.age = -1
+  	assert pet.invalid?
+  	assert_equal ["must be greater than 0"], pet.errors[:age]
+
+  	pet.age = 0
+  	assert pet.invalid?
+  	assert_equal ["must be greater than 0"], pet.errors[:age]
+
+  	pet.age = 1
+  	assert pet.valid?
+  end
+
 end
