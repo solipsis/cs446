5d9d31f3fe7924c816a968e1a6d019b80e6569a2
David Alexander <dalexand@mymail.mines.edu>
2014-02-04 11:41:47 -0700

    basic shelter catalogue implemented

--- pets/config/routes.rb
+++ pets/config/routes.rb
@@ -1,11 +1,12 @@
 Pets::Application.routes.draw do
+  get "shelter/index"
   resources :pets
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'shelter#index', as: 'shelter'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
