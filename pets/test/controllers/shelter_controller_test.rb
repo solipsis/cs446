f59add04192eebc93f5c82fc628a793cd796caef
David Alexander <dalexand@mymail.mines.edu>
2014-02-05 03:12:44 -0700

    shelter controller unit testing

--- pets/test/controllers/shelter_controller_test.rb
+++ pets/test/controllers/shelter_controller_test.rb
@@ -4,6 +4,9 @@ class ShelterControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimum: 2
+    assert_select '#main .entry', 3
+
   end
 
 end
