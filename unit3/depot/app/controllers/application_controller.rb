1402dc1c9999d6c6b5d0d93970c8ba63a6d2e878
David Alexander <dalexand@mymail.mines.edu>
2014-02-11 00:52:14 -0700

    whitelisted commands

--- unit3/depot/app/controllers/application_controller.rb
+++ unit3/depot/app/controllers/application_controller.rb
@@ -7,7 +7,18 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  	def authorize
+  		unless User.find_by(id: session[:user_id])
+  			redirect_to login_url, notice: "please log in"
+  		end
+  	end
+
 end
