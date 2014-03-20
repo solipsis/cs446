47aa9c1661219fef2333da30135a52e4e7cdb8a0
David Alexander <dalexand@mymail.mines.edu>
2014-03-20 09:43:07 -0600

    book listing now populated by database Part 1 completed

--- books_partI/books/bookTable.rb
+++ books_partI/books/bookTable.rb
@@ -3,12 +3,13 @@
 require 'rack'
 require 'csv'
 require 'ERB'
+require 'sqlite3'
 
 class BookTable
 	
 	def initialize()
     	@filename = "bookList.csv"
-		@books = []	
+		@books = []
 	end
 
 	def call(env)
@@ -25,7 +26,6 @@ class BookTable
         		file = env["PATH_INFO"][1..-1]
         		return [200, {"Content-Type" => "text/css"}, [File.open(file, "rb").read]]
 			when /\/list.*/
-				loadBooks(response, @filename)
 				drawTable(response, request)
 				
 			when /\/form.*/
@@ -43,67 +43,16 @@ class BookTable
 
 	def render_form(response)
 		response.write(ERB.new(File.read("form.html.erb")).result(binding))
-		#puts ERB.new(File.read("form.html.erb")).result(binding)
-		#ERB.new(File.read("form.html.erb")).result(binding)
-		#response(ERB.new(File.read("form.html.erb")).result(binding))
-		# response.write('<form name="input" action="http://localhost:8080/list" method="get">')
-		# response.write('<select name="sort">')
-		# response.write('<option value="title">Title</option>')
-		# response.write('<option value="author">Author</option>')
-		# response.write('<option value="language">Language</option>')
-		# response.write('<option value="year">Year</option>')
-		# response.write('</select>')
-		# response.write('<input type="submit" value="display list">')
-		# response.write('</form>')
+		
 	end
 
 	# draws the html table with the book info
 	def drawTable(response, request)
 		response.write(ERB.new(File.read("list.html.erb")).result(binding))
-		# sortBy = request.GET["sort"]
-		# @books = @books.sort_by { |x| x[:"#{sortBy}"]}
-
-		# response.write("<table>")
-		# response.write("<tr>")
-		# response.write("<td>Rank</td>")
-		# response.write("<td>Title</td>")
-		# response.write("<td>Author</td>")
-		# response.write("<td>Language</td>")
-		# response.write("<td>Year</td>")
-		# response.write("<td>Copies</td>")
-		# response.write("</tr>")
-		# @books.each do |book|
-		# 	response.write("<tr>")
-		# 	response.write("<td>#{book[:rank]}</td>")
-		# 	response.write("<td>#{book[:title]}</td>")
-		# 	response.write("<td>#{book[:author]}</td>")
-		# 	response.write("<td>#{book[:language]}</td>")
-		# 	response.write("<td>#{book[:year]}</td>")
-		# 	response.write("<td>#{book[:copies]}</td>")
-		# 	response.write("</tr>")
-		# end
-		# response.write("</table>")
+		
 	end
 
 
-	# reads the list of books from a comma separated file
-	def loadBooks(response, filename)
-		CSV.foreach(File.path(filename)) do |col|
-			book = {}
-			book[:title] = col[0]
-			book[:author] = col[1]
-			book[:language] = col[2]
-			book[:year] = col[3]
-			book[:copies] = col[4]
-			@books.push(book)
-		end
-		@books = @books.sort_by { |x| x[:copies].to_i }.reverse
-		x = 1
-		@books.each do |book|
-			book[:rank] = x
-			x = x + 1
-		end
-	end
 end
 
 Signal.trap('INT') {
